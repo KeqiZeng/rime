572200a51fb541de976abc3fd62049f4f2929d8e
Ketch Zeng <zengkeqi1999@163.com>
2022-02-13 14:06:17 +0800

    first commit

---
+++ rime.lua
@@ -0,0 +1,34 @@
+--[[
+librime-lua 样例
+调用方法：
+在配方文件中作如下修改：
+```
+  engine:
+    ...
+    translators:
+      ...
+      - lua_translator@lua_function3
+      - lua_translator@lua_function4
+      ...
+    filters:
+      ...
+      - lua_filter@lua_function1
+      - lua_filter@lua_function2
+      ...
+```
+其中各 `lua_function` 为在本文件所定义变量名。
+--]]
+
+-- I. translators:
+
+-- date_translator: 将 `date` 翻译为当前日期
+-- 详见 `lua/date.lua`:
+date_translator = require("date")
+
+-- time_translator: 将 `time` 翻译为当前时间
+-- 详见 `lua/time.lua`
+time_translator = require("time")
+
+-- II. filters:
+
+-- III. processors:
