d51663f8e26a9395d83424a19743d3486f2a1558
Keqi Zeng <ketchz0206@outlook.com>
2024-12-16 23:27:19 +0100

    Add radical and lua scripts

--- rime.lua
+++ rime.lua
@@ -20,14 +20,10 @@ librime-lua 样例
 --]]
 
 -- I. translators:
-
--- date_translator: 将 `date` 翻译为当前日期
--- 详见 `lua/date.lua`:
 date_translator = require("date")
-
--- time_translator: 将 `time` 翻译为当前时间
--- 详见 `lua/time.lua`
 time_translator = require("time")
+lunar_translator = require("lunar")
+number_translator = require("number")
 
 -- II. filters:
 
